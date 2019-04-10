1e5ce32e574a5a666056e511c1011703978e3b2c
liuqi <timeless_lq@163.com>
2019-04-10 15:52:33 +0800

    High Availability spring cloud config

--- config-server/src/main/java/com/qseven/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/qseven/configserver/ConfigServerApplication.java
@@ -3,10 +3,16 @@ package com.qseven.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
+
+    /*
+     * 根据Git仓库配置文件名称命名，这里正确的访问方式：http://localhost:8762/config-client/dev
+     * */
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
